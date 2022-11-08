a672e84d4bac6c9537505ddbd510572af94458b9
Clemens Schmid <clemens@nevrome.de>
2022-11-08 09:17:54 +0100

    removed all NA values from input data for simplicity

--- data/data_prep.R
+++ data/data_prep.R
@@ -2,16 +2,17 @@ library(magrittr)
 
 peng <- palmerpenguins::penguins
 
-peng_with_id <- peng %>%
+peng_prepped <- peng %>%
+  dplyr::filter(!dplyr::if_any(.fns = is.na)) %>%
   tibble::add_column(., id = 1:nrow(.), .before = "species")
 
-peng_with_id %>%
+peng_prepped %>%
   dplyr::slice_sample(n = 300) %>%
   dplyr::arrange(id) %>%
   dplyr::select(-bill_length_mm, -bill_depth_mm) %>%
   readr::write_csv("data/penguins.csv")
 
-peng_with_id %>%
+peng_prepped %>%
   dplyr::slice_sample(n = 300) %>%
   dplyr::arrange(id) %>%
   dplyr::select(id, bill_length_mm, bill_depth_mm) %>%
